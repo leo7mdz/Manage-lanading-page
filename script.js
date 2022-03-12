5ec66c6e6c4716e943bf38191cb781241f2c168f
leonardo <leo7mendoza7@gmail.com>
2022-03-12 11:55:26 -0300

    add slider

--- script.js
+++ script.js
@@ -15,3 +15,58 @@ window.addEventListener("scroll", (e) => {
     menu.classList.remove("menu-is-active");
   }
 });
+
+/*swiper*/
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  //autoplay
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  /*  scrollbar: {
+    el: ".swiper-scrollbar",
+  }, */
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  breakpoints: {
+    620: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
+
+/* end swiper */
+
+/*form  */
